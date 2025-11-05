7d5e5d15863739babe1d1d7df99f188f085f3be1
Fabio Caccamo <fabio.caccamo@gmail.com>
2025-11-05 23:25:15 +0100

    Create apps.py

---
+++ src/django_validate_email_strict/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoValidateEmailStrictConfig(AppConfig):
+    name = "django_validate_email_strict"
+    verbose_name = "Django Validate Email Strict"
+    default_auto_field = "django.db.models.BigAutoField"
